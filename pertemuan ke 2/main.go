f2ee1ceb1ff60873d42665740d9d4ebd1dab447d
alifian438 <alifian290126@gmail.com>
2022-10-05 10:29:51 +0700

    firts commit

---
+++ pertemuan ke 2/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("ini project 2")
+
+	if nilaiStudent := 38; nilaiStudent > 7 {
+		fmt.Println("lulus")
+	}
+	if nilaiStudent := 38; nilaiStudent < 7 {
+		fmt.Println("lulus")
+	}
+
+}
